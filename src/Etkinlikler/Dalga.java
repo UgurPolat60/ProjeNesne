4f2835f2c8e2aaa3e4c64eed770c89a3aaf87793
UgurPolat <ugurplt8@gmail.com>
2025-03-19 16:39:26 +0300

    initial commit

---
+++ src/Etkinlikler/Dalga.java
@@ -0,0 +1,19 @@
+package Etkinlikler;
+
+import java.util.ArrayList;
+
+public class Dalga {
+	private ArrayList<Integer> dusmanliste;
+
+	public Dalga(ArrayList<Integer> dusmanliste) {
+		this.dusmanliste = dusmanliste;
+	}
+
+	
+	public ArrayList<Integer> getdusmanliste() {
+		return dusmanliste;
+	}
+
+	
+
+}
